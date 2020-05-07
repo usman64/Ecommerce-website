e477c648ae8d0ff92720afcdf2ac89a1421ce515
usman64 <usmantariq65@outlook.com>
2020-05-08 02:36:39 +0500

    Added dynamic Routing, withRouter() to counter Prop drilling, spread operator, history.push(), browserRouter, Switch, Route

--- lessons/3/components/menu-item/menu-item.component.jsx
+++ lessons/3/components/menu-item/menu-item.component.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+
 import './menu-item.styles.scss';
 
 //- Menu-Item styling
 //- used self-closing for background image <div /> because we didn't want other things to scale, but the background image only
 //- absolute positioning of content bcz of background image
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
     <div
       style={{
         backgroundImage: `url(${imageUrl})`,
@@ -20,4 +25,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
